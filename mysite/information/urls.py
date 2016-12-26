9b640092d605ba16518f0f3ea83f9f693a608de0
qjzhzw <450388261@qq.com>
2016-12-26 22:15:50 +0800

    完成了注册详细信息部分（除了图片上传）

--- mysite/information/urls.py
+++ mysite/information/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns=[
 	url(r'^register/', views.register, name='register'),
+	url(r'^register_information/', views.register_information, name='register_information/'),
 	url(r'^login/', views.login, name='login'),
 ]
--- mysite/information/views.py
+++ mysite/information/views.py
@@ -31,16 +31,49 @@ def register(request):
 		
 		return HttpResponse(simplejson.dumps(data))
 
+
+def register_information(request):
+	if request.method == 'POST':
+		flag = request.POST['identification']#获取用户名
+		data1 = request.POST['nickname']#获取昵称
+		data2 = request.POST['sex']#获取性别
+		data3 = request.POST['birthday']#获取生日
+		data4 = request.POST['constellation']#获取星座
+		data5 = request.POST['hobby']#获取爱好
+		data6 = request.POST['email']#获取邮箱
+		data7 = request.POST['motto']#获取签名
+		data8 = request.POST['head']#获取头像
+		result = information.objects.filter(identification = flag).last()
+		
+		data={}
+		try:
+			result.nickname = data1
+			result.sex = data2
+			result.birthday = data3
+			result.constellation = data4
+			result.hobby = data5
+			result.email = data6
+			result.motto = data7
+			result.head = data8
+			result.save()
+			data['status'] = 'success'
+		except Exception, e:
+			print e
+			data['status'] = 'error'
+		
+		return HttpResponse(simplejson.dumps(data))
+
+
 def login(request):
 	if request.method == 'POST':
 		data1 = request.POST['identification']#获取用户名
 		data2 = request.POST['password']#获取密码
-		result = information.objects.filter(identification = data1)
+		result = information.objects.filter(identification = data1)#注意这里得到的是QuerySet类型
 		
 		data = {}
 		if(len(result) == 0):
 			data['status'] = 'not_existed'
-		elif(cmp(result.last().password, data2) == 0):
+		elif(cmp(result.last().password, data2) == 0):#这里得到的才是information类型
 			data['status'] = 'success'
 		else:
 			data['status'] = 'not_matched'
